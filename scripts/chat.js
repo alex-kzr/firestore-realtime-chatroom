4453ec9a494dc0febb232b5297eb4cd24b1fce13
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-04-01 16:40:42 +1000

    Add addChat method

--- scripts/chat.js
+++ scripts/chat.js
@@ -9,4 +9,27 @@ class Chatroom {
         this.username = username;
         this.chats = db.collection('chats');
     }
+    async addChat(message){
+        // format a chat object
+        const now = new Date();
+        const chat = {
+            message,
+            username: this.username,
+            room: this.room,
+            create_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+        // save the chat document
+        const response = await this.chats.add(chat);        
+        return response;
+    }
 }
+
+const chatroom = new Chatroom('gaming', 'alex');
+
+chatroom.addChat('hello everyone').then(() => {
+    console.log('chat added');
+}).catch(err => {
+    console.log(err);
+});
+
+console.log(chatroom);
